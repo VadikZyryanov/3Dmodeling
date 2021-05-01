0975d974427cc1f295c1b896bb40f948db57382c
vadik <vadik94_1rec@bk.ru>
2021-05-01 18:02:24 +0500

    Выполненное задание урока №24

--- js/main.js
+++ js/main.js
@@ -288,53 +288,82 @@ window.addEventListener('DOMContentLoaded', () => {
 
     //регулярные выражения
     const regularExpressions = () => {
-        const inputCalcItem = document.querySelectorAll('input.calc-item'),
-            inputTopForm = document.querySelectorAll('input.top-form'),
+        const formOneName = document.getElementById('form1-name'),
+            formTwoName = document.getElementById('form2-name'),
+            formThreeName = document.getElementById('form3-name'),
+            formOneEmail = document.getElementById('form1-email'),
+            formTwoEmail = document.getElementById('form2-email'),
+            formThreeEmail = document.getElementById('form3-email'),
+            formOnePhone = document.getElementById('form1-phone'),
+            formTwoPhone = document.getElementById('form2-phone'),
+            formThreePhone = document.getElementById('form3-phone'),
             inputMessage = document.getElementById('form2-message');
 
-        const check = (elem) => {
-            elem.value = elem.value.replace(/\s+/g, ' ');
-            elem.value = elem.value.replace(/\-+/g, '-');
-            elem.value = elem.value.replace(/^-+|-+$/g, '');
-            elem.value = elem.value.trim();
+        const checkName = (input) => {
+            input.value = input.value.replace(/[^А-Яа-яЁё|\-|\s]/g, '');
+            input.addEventListener('blur', () => {
+                input.value = input.value.replace(/\s+/g, ' ');
+                input.value = input.value.replace(/\-+/g, '-');
+                input.value = input.value.replace(/^-+|-+$/g, '');
+                input.value = input.value.trim();
+                let number = input.value.split(' ');
+                for (let i = 0; i < number.length; i++) {
+                    number[i] = number[i][0].toUpperCase() + number[i].slice(1).toLowerCase();
+                }
+                input.value = number.join(' ');
+            });
         };
 
-        inputCalcItem.forEach((input) => {
-            input.addEventListener('input', () => {
-                input.value = input.value.replace(/\D/g, '');
+        const checkEmail = (input) => {
+            input.value = input.value.replace(/[^\w|\@|\-|\_|\.|\!|\~|\*|\']/g, '');
+            input.addEventListener('blur', () => {
+                input.value = input.value.replace(/\-+/g, '-');
+                input.value = input.value.replace(/^-+|-+$/g, '');
             });
+        };
+
+        const checkPhone = (input) => {
+            input.value = input.value.replace(/[^0-9|\(|\)|\-]/g, '');
+            input.addEventListener('blur', () => {
+                input.value = input.value.replace(/\-+/g, '-');
+                input.value = input.value.replace(/^-+|-+$/g, '');
+            });
+        }
+
+        formOneName.addEventListener('input', () => {
+            checkName(formOneName);
         });
 
-        inputTopForm.forEach((input) => {
-            input.addEventListener('input', (event) => {
-                let target = event.target;
-                if (target.matches('.form-email')) {
-                    input.value = input.value.replace(/[^\w|\@|\-|\_|\.|\!|\~|\*|\']/g, '');
-                    input.addEventListener('blur', () => {
-                        input.value = input.value.replace(/\-+/g, '-');
-                        input.value = input.value.replace(/^-+|-+$/g, '');
-                    });
-                } else if (target.matches('.form-phone')) {
-                    input.value = input.value.replace(/[^0-9|\(|\)|\-]/g, '');
-                    input.addEventListener('blur', () => {
-                        input.value = input.value.replace(/\-+/g, '-');
-                        input.value = input.value.replace(/^-+|-+$/g, '');
-                    });
-                } else {
-                    input.value = input.value.replace(/[^А-Яа-яЁё|\-|\s]/g, '');
-                    input.addEventListener('blur', () => {
-                        input.value = input.value.replace(/\s+/g, ' ');
-                        input.value = input.value.replace(/\-+/g, '-');
-                        input.value = input.value.replace(/^-+|-+$/g, '');
-                        input.value = input.value.trim();
-                        let number = input.value.split(' ');
-                        for (let i = 0; i < number.length; i++) {
-                            number[i] = number[i][0].toUpperCase() + number[i].slice(1).toLowerCase();
-                        }
-                        input.value = number.join(' ');
-                    });
-                }
-            });
+        formTwoName.addEventListener('input', () => {
+            checkName(formTwoName);
+        });
+
+        formThreeName.addEventListener('input', () => {
+            checkName(formThreeName);
+        });
+
+        formOneEmail.addEventListener('input', () => {
+            checkEmail(formOneEmail);
+        });
+
+        formTwoEmail.addEventListener('input', () => {
+            checkEmail(formTwoEmail);
+        });
+
+        formThreeEmail.addEventListener('input', () => {
+            checkEmail(formThreeEmail);
+        });
+
+        formOnePhone.addEventListener('input', () => {
+            checkPhone(formOnePhone);
+        });
+
+        formTwoPhone.addEventListener('input', () => {
+            checkPhone(formTwoPhone);
+        });
+
+        formThreePhone.addEventListener('input', () => {
+            checkPhone(formThreePhone);
         });
 
         inputMessage.addEventListener('input', () => {
